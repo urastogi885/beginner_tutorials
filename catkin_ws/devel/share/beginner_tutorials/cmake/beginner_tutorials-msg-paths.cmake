d94635535c38aaa1162fe4742c051cf643261080
umang885 <umang.rastogi96@gmail.com>
2019-10-28 14:21:40 -0400

    Delete unnecessary files of catkin workspace

--- catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/umang/Documents/808x/assignments/beginner_tutorials/catkin_ws/src/beginner_tutorials/msg")
-set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
